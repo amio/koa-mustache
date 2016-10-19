1b10ca6ba90497347e23ba7817cb751d081bc66e
Amio <amio.cn@gmail.com>
2016-10-19 13:09:15 +0800

    (feat) Support partials.

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ const attempts = require('attempts')
 const Mustache = require('mustache')
 
 const defaultOptions = {
-  extension: 'mustache',
-  debug: false
+  partialsDir: '_partials', // A directory inside rootpath, for partial files.
+  extension: 'mustache',    // The file extension for mustache templates.
+  debug: false              // Disable debug mode.
 }
 
-module.exports = function (rootpath, opts = defaultOptions) {
+module.exports = function (rootpath, opts) {
+  opts = Object.assign(opts, { root: rootpath })
   return function mustache (ctx, next) {
     if (ctx.method === 'GET') {
       const p = path.join(rootpath, ctx.path)
-      const content = render(p, opts.extension, opts.debug)
+      const content = render(p, Object.assign(defaultOptions, opts))
       if (content) {
         ctx.body = content
       } else {
@@ -30,15 +32,19 @@ module.exports = function (rootpath, opts = defaultOptions) {
  * @param  {Boolean} debug
  * @return {String|undefined}
  */
-function render (pathname, ext, debug) {
-  const p = resolveAvailableTemplate(pathname, ext)
+function render (pathname, opts) {
+  const p = resolveAvailableTemplate(pathname, opts.extension)
   if (!p) return ''
-  debug && console.log('FOUND TEMPLATE:', p + ext)
 
-  const tmpl = fs.readFileSync(p + ext, 'utf-8')
+  opts.debug && console.log('\nFOUND TEMPLATE:', p + opts.extension)
+
+  const template = fs.readFileSync(p + opts.extension, 'utf-8')
+  const partials = parsePartials(template, opts)
   const data = attempts.sync(p => require(p), [p + 'json', p + 'data.js'])
-  debug && console.log('FOUND DATA:', data)
-  return Mustache.render(tmpl, data)
+
+  opts.debug && console.log('FOUND DATA:', data)
+
+  return Mustache.render(template, data, partials)
 }
 
 /**
@@ -60,4 +66,23 @@ function resolveAvailableTemplate (pathname, ext) {
     path.join(pathname + '.' + ext),
     path.join(pathname, 'index.' + ext)
   ])
+}
+
+function parsePartials (tmpl, opts) {
+  const partialMatches = tmpl.match(/({{> )\w+(}})/g)
+
+  if (!partialMatches) {
+    return {}
+  } else {
+    const partials = {}
+    attempts.sync(part => {
+      const name = part.match(/\w+/)[0]
+      const filename = name + '.' + opts.extension
+      const filepath = path.join(opts.root, opts.partialsDir, filename)
+      partials[name] = fs.readFileSync(filepath, { encoding: 'utf8' })
+
+      opts.debug && console.log('USE PARTIAL:', filename) // todo
+    }, partialMatches)
+    return partials
+  }
 }
